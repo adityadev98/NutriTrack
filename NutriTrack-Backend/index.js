fc81ad8f0091406506c2a5c6a065916a46438228
Gayathri Balasundaram <Gayathri@Akshayas-MacBook-Pro.local>
2025-02-06 16:36:34 -0500

    added backend folders for meal log

--- NutriTrack-Backend/index.js
+++ NutriTrack-Backend/index.js
@@ -1,29 +1,25 @@
-import express from "express";
-import dotenv from "dotenv"; 
-import {connectDB} from "./config/db.js";
-//import productRoutes from "./routes/product.route.js";
-import path from "path";
+//entry point for api
+import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+import cors from 'cors';
+
+import nutriRoutes from './routes/nutriRoutes.js';
 
 dotenv.config();
 
+console.log(process.env.MONGO_URI);
+const PORT = process.env.PORT || 8001
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); //express.json is an inbuilt middleware that helps to accept JSOn data in the req.body
+app.use(cors());
 
-const __dirname = path.resolve();
+app.use("/",nutriRoutes)
 
-//app.use("/api/products", productRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/NutriTrack-Frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "NutriTrack-Frontend", "dist", "index.html"));
-	});
-}
-
-app.listen(5000, () => {
+app.listen(PORT,()=>{
     connectDB();
-    console.log("Server Started at http://localhost:"+PORT);
+    console.log("Server is up and running @ http://localhost:",PORT)
 });
 
