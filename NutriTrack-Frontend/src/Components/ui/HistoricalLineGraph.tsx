cd31cc3892ff4676cb93c17541f7e2d3064f5e5e
Azadan Bhagwagar <abhagwag@uwaterloo.ca>
2025-02-12 15:13:43 -0500

    Added nutrient filtering for historical nutrient graph

--- NutriTrack-Frontend/src/Components/ui/HistoricalLineGraph.tsx
+++ NutriTrack-Frontend/src/Components/ui/HistoricalLineGraph.tsx
@@ -1,8 +1,25 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
-import React from 'react';
+import React, { useState } from 'react';
 import { convertEpochToFormattedDate }  from '../../Services/historicalViewServices.js';
 
 const HistoricalLineGraph: React.FC = ({ historicalData }) => {
+
+    const [visibleLines, setVisibleLines] = useState({
+      totalCalories: true,
+      totalProtein: true,
+      totalFat: true,
+      totalFiber: true,
+      totalCarbohydrate: true,
+    });
+
+    const handleLegendClick = (e: any) => {
+      const { dataKey } = e;
+      setVisibleLines((prev) => ({
+        ...prev,
+        [dataKey]: !prev[dataKey], // Toggle visibility
+      }));   
+    };
+
     return (
         <div className="flex justify-center items-center w-full h-full">
         <LineChart
@@ -17,17 +34,19 @@ const HistoricalLineGraph: React.FC = ({ historicalData }) => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="aggTime" angle="-45" tickMargin="20" tickFormatter={convertEpochToFormattedDate}
+          <XAxis includeHidden dataKey="aggTime" angle="-45" tickMargin="20" tickFormatter={convertEpochToFormattedDate}
           scale="time" type="number" domain={['dataMin','dataMax + 1000']} />
-          <YAxis yAxisId="left" label={{ value: "Calories (kCal)", angle: -90, position: 'left' }} />
-          <YAxis yAxisId="right" orientation="right" label={{ value: "Nutrients (g)", angle: -90, position: 'insideRight' }} />
+          <YAxis includeHidden yAxisId="left" label={{ value: "Calories (kCal)", angle: -90, position: 'left' }} />
+          <YAxis includeHidden yAxisId="right" orientation="right" label={{ value: "Nutrients (g)", angle: -90, position: 'insideRight' }} />
           <Tooltip labelFormatter={convertEpochToFormattedDate} />
-          <Legend wrapperStyle={{ position: 'relative' }}/>
-          <Line connectNulls yAxisId="left" type="linear" dataKey="totalCalories" stroke="#4E79A7" activeDot={{ r: 8 }} />
-          <Line connectNulls yAxisId="right" type="linear" dataKey="totalProtein" stroke="#F28E2B" />
-          <Line connectNulls yAxisId="right" type="linear" dataKey="totalFat" stroke="#E15759" />
-          <Line connectNulls yAxisId="right" type="linear" dataKey="totalCarbohydrate" stroke="#76B7B2" />
-          <Line connectNulls yAxisId="right" type="linear" dataKey="totalFiber" stroke="#B07AA1" />
+          <Legend wrapperStyle={{ position: 'relative' }} onClick={handleLegendClick} />
+
+          <Line connectNulls yAxisId="left" type="linear" dataKey="totalCalories" stroke="#4E79A7" activeDot={{ r: 8 }} hide={!visibleLines.totalCalories} />
+          <Line connectNulls yAxisId="right" type="linear" dataKey="totalProtein" stroke="#F28E2B" hide={!visibleLines.totalProtein} />
+          <Line connectNulls yAxisId="right" type="linear" dataKey="totalFat" stroke="#E15759" hide={!visibleLines.totalFat} />
+          <Line connectNulls yAxisId="right" type="linear" dataKey="totalCarbohydrate" stroke="#76B7B2" hide={!visibleLines.totalCarbohydrate} />
+          <Line connectNulls yAxisId="right" type="linear" dataKey="totalFiber" stroke="#B07AA1" hide={!visibleLines.totalFiber} />
+          
         </LineChart>
       </div>
     );
