a312c987c2ab53e0621b13d3fb6d5fb2ea96c675
adityadev98 <aditya.dev43@gmail.com>
2025-03-17 21:57:29 -0400

    Removed Navlinks

--- NutriTrack-Frontend/src/Constants/index.ts
+++ NutriTrack-Frontend/src/Constants/index.ts
@@ -11,28 +11,6 @@ export {
 
 }
 
-export const navLinks = [
-  {
-    id: "home",
-    title: "Home",
-  },
-  {
-    id: "features",
-    title: "Features",
-  },
-  {
-    id: "pricing",
-    title: "Pricing",
-  },
-  {
-    id: "faq",
-    title: "FAQs",
-  },
-  {
-    id: "testimonials",
-    title: "Testimonials",
-  },
-];
 
 export const features = [
   {
