f241cb412fff369c9e29b269ac8ac0734bb4248c
adityadev98 <aditya.dev43@gmail.com>
2025-03-17 22:08:14 -0400

    Removed DashNavLinks

--- NutriTrack-Frontend/src/Constants/index.ts
+++ NutriTrack-Frontend/src/Constants/index.ts
@@ -91,40 +91,4 @@ export const socialMedia = [
 },
 ];
 
-export const DashNavLinks = [
-  {
-    id: "dashboard",
-    title: "Dashboard",
-    icon: FiHome,
-  },
-  {
-    id: "track",
-    title: "Track",
-    icon: FiHome
-  },
-  {
-    id: "mealsConsumed",
-    title: "Meals Consumed",
-    icon: MdNoMeals ,
-  },
-  {
-    id: "customFood",
-    title: "Custom Food",
-    icon: FiHome,
-  },
-  {
-    id: "dailydashboard",
-    title: "Daily Dashboard",
-    icon: MdDashboardCustomize,
-  },
-  {
-    id: "historical",
-    title: "Historical",
-    icon: GoGraph,
-  },
-  {
-    id: "profile-setup",
-    title: "Profile",
-    icon: CgProfile ,
-  },
-];                 
+              
